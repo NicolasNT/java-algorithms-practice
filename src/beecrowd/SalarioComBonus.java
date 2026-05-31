0d9c6ba3f39391b8d680c2b936723d0a33946a6a
Nicolas Taniguchi <nicolas.taniguchi@outlook.com>
2026-06-01 06:28:08 +0900

    feat:beecrowd - resolve exercicio 1009

---
+++ src/beecrowd/SalarioComBonus.java
@@ -0,0 +1,36 @@
+package beecrowd;
+import java.util.Scanner;
+
+/*
+1009 - Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas
+por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
+informar o total a receber no final do mês, com duas casas decimais.
+
+Entrada
+
+O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double)
+com duas casas decimais, representando o salário fixo do vendedor e montante total das vendas efetuadas
+por este vendedor, respectivamente.
+
+Saída
+
+Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+ */
+
+public class SalarioComBonus {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+
+        String nomeVendedor = leitura.nextLine();
+        double salarioFixo = leitura.nextDouble();
+        double totalVendas = leitura.nextDouble();
+        double comissao = totalVendas * 0.15;
+
+        double totalReceber = salarioFixo + comissao;
+
+        System.out.printf("TOTAL = R$ %.2f%n", totalReceber);
+
+
+    }
+}
